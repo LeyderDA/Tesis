8a37cb1cd16c4f77bf07aa4c986eece15d4ba7d6
LEYDER20 <leydertesis2020@gmail.com>
2020-10-30 12:12:16 -0500

    Mejorado el diseño de las firmas

--- resources/views/CrearFirma.blade.php
+++ resources/views/CrearFirma.blade.php
@@ -1,8 +1,14 @@
 
+<div style="height:30px; width:100%; background-color:#dadada"></div>
+<div style="height:6px; width:100%; background-color:#ad3333"></div>
+
 <!DOCTYPE html>
 <html>
 	<head>
-		<meta charset="utf-8">
+        <meta charset="utf-8">
+        <br>
+        <br>
+        <br>
         <title>Crear Firma</title>
         <link href="{{ asset('css/firma.css') }}" rel="stylesheet">
 		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
@@ -10,15 +16,24 @@
 		<script src="./js/bezier.js"></script>
 		<script src="./js/jquery.signaturepad.js"></script> 		
 		<script type='text/javascript' src="https://github.com/niklasvh/html2canvas/releases/download/0.4.1/html2canvas.js"></script>
-		<script src="./js/json2.min.js"></script>				
+        <script src="./js/json2.min.js"></script>	
+        
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    
+
+
+
+
+
+
 		<style type="text/css">
 			body{
 				font-family:monospace;
 				text-align:center;
 			}
 			#btnSaveSign {
-				color: #fff;
-				background: #1006a0;
+				color: rgb(0, 0, 0);
+				background: #000000;
 				padding: 5px;
 				border: none;
 				border-radius: 5px;
@@ -26,6 +41,7 @@
 				margin-top: 10px;
 			}
 			#signArea{
+            
 				width:304px;
 				margin: 50px auto;
 			}
@@ -34,12 +50,14 @@
 				margin: auto;
 			}
 			.sign-preview {
+                
 				width: 150px;
 				height: 50px;
 				border: solid 1px #CFCFCF;
 				margin: 10px 5px;
 			}
 			.tag-ingo {
+                
 				font-family: cursive;
 				font-size: 12px;
 				text-align: left;
@@ -49,17 +67,36 @@
 	</head>
 	<body>
 
-		<h2>Crea tu firma ahora</h2>
+        
+        <font face="arial">
+            <h2>¡CREA TU FIRMA AHORA!</h2>
+        </font>
+
+       
 		
 		<div id="signArea" >
-			<h2 class="tag-ingo">Ponga la firma debajo</h2>
+            <br>
+            <center>
+                <div>
+                    <h2 class="tag-ingo">Ponga la firma debajo</h2>
+                </div>
+            </center>
+            
+                
+          
+			
 			<div id="figuras" class="sig sigWrapper" style="height:auto;">
 				<div class="typed"></div>
 				<canvas class="sign-pad" id="sign-pad" width="300" height="100"></canvas>
             </div>
             <br>
             <br>
-            <input type="button" class="btn tw" id="btnSave2" value="Descargar imagen">
+            <font face="arial">
+                <input type="button" class="btn btn-primary" id="btnSave2" value="Descargar Firma">
+            </font>
+            
+
+           
 		</div>
 		
 		
@@ -76,10 +113,17 @@
 		}
 		?>
         </div>
-        <h4 class="mb-2 card-title">¿Te Equivocaste? 
+        
            
+            <font face="arial">
+                <h4 class="mb-2 card-title">¿Te Equivocaste? 
+            </font>
         </h4> 
-        <a href="/CrearFir" >Resetear Firma</a>
+        <font face="arial">
+            <a href="/CrearFir" >Resetear Firma</a>
+        </font>
+
+       
 		
 		<script>
             document.getElementById('figuras').contentEditable = 'true';document.getElementById('figuras').designMode='on';
@@ -138,7 +182,12 @@
     </body>
     <br>
     <br>
-    <center> 
-        <a href="/home"  class="btn btn-primary"  >Salir</a>
-      </center> 
+    <font face="arial">
+        <center> 
+            <a href="/home"  class="btn btn-primary"  >Salir</a>
+          </center> 
+    </font>
+
+   
 </html>
+
