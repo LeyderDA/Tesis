454a0d209b3943e9951b2922ec3e633a7450eb28
LEYDER20 <leydertesis2020@gmail.com>
2020-11-01 23:19:06 -0500

    ya redirecciona al archivo

--- resources/views/ArchivosGestiones.blade.php
+++ resources/views/ArchivosGestiones.blade.php
@@ -16,14 +16,14 @@
                               <center>
                                 <?php $content = DB::table('archivos_gestiones')->select('id','archivoGe')->where('ges_id',$Gest->id)->get(); ?>                              
                                 @foreach($content as $contenido)                             
-                                <h1 class="mb-2 card-title"><a href="/{{$contenido->archivoGe}}" target="_blank"></h1>                           
+                                <h1 class="mb-2 card-title"><a href="/storage/GestionArchivos/{{$contenido->archivoGe}}" target="_blank"></h1>                           
                                   <?php 
                                    $pizza  = $contenido->archivoGe;
-                                   $porciones = explode("/", $pizza);
+                                   $porciones = explode("_", $pizza);
                                    $part1 = $porciones[0]; 
                                    $part2 = $porciones[1];
-                                   $part3 = $porciones[2];
-                                     echo $part3; // porción3
+                                   
+                                     echo $part2; // porción3
                                    ?>
                                </a>          
                               @endforeach 
