8ebb38b082d27ac11653965c53f17e6992ef32aa
LEYDER20 <leydertesis2020@gmail.com>
2020-11-17 22:59:18 -0500

    ya creo que funciona

--- app/Http/Controllers/NotificacionesController.php
+++ app/Http/Controllers/NotificacionesController.php
@@ -1,16 +1,40 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Http\Controllers\request_id;
 use Illuminate\Http\Request;
 use App\Notificaciones;
+use App\User;
 class NotificacionesController extends Controller
 {
 
     public function index()
     {
-        $areas = Notificaciones::all();
+       
+
+
+        $id = (new request_id)->get_id();  
+        $areas = Notificaciones::all()->where('id_usuario','=',$id);
         return  response()->json($areas);
+
+        // $mensj = Notificaciones::
+        // select('notificaciones.*')
+        // ->orderBy('notificaciones.id', 'asc')
+        // ->where('notificaciones.id_usuario','=',$id)
+        // ->get();
+        
+        //      if ($request->ajax()) {
+        //       foreach ($mensj as $me) {
+        //         $me->notificaciones;
+        //      }
+        //      return $mensj;
+        //      } else {
+        //       return  response()->json($mensj);
+        //      }
+
+
+
+
     }
     public function destroy($id)
     {
